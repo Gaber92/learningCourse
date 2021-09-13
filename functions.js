d35760b8ef496ae93b0f96a81742fc5b412e2f3b
Luka Gaber <luka.gaber@dlabs.si>
2021-09-13 18:29:44 +0200

    add functions

---
+++ functions.js
@@ -0,0 +1,9 @@
+function openFacebook(x) {
+  window.open("https://www.facebook.com");   // The function returns the product of p1 and p
+}
+function openInstagram(x) {
+  window.open("https://www.instagram.com");   // The function returns the product of p1 and p
+}
+function openLinkedIn(x) {
+  window.open("https://www.linkedin.com");   // The function returns the product of p1 and p
+}
