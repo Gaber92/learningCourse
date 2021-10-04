f6dc60324d803122980d87efe49e7bfdfbefd522
Luka Gaber <luka.gaber@dlabs.si>
2021-10-04 14:29:48 +0200

    fix

--- Python/homework2.py
+++ Python/homework2.py
@@ -2,6 +2,6 @@ secret = int(73)
 guess = int(input("Guess what is the secret lottery number: "))
 
 if guess == secret:
-    print ("Congratulations you have won 1 billion euros!")
+    print("Congratulations you have won 1 billion euros!")
 if guess != secret:
-    print ("Your guess " + str(guess) + " was incorrect. Better luck next time!")
+    print("Your guess " + str(guess) + " was incorrect. Better luck next time!")
