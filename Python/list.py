1dd8462dad3be3d31c14f54cc43ebd16543bd6f8
Luka Gaber <luka.gaber@dlabs.si>
2021-10-11 16:37:07 +0200

    explore list and dictionaries

---
+++ Python/list.py
@@ -0,0 +1,48 @@
+randomText = "Lololo"
+number = 1234
+floatNumber = 2432.432523
+human = True
+
+# lists
+
+randomArray = []
+numbers = [1, 2, 3, 4, 5]
+strings = ["GDE", "CDE", "AAD"]
+mixed = [1, "foekfoko", True]
+
+
+numbers.append(6)
+# print(numbers[1:2])
+# print(numbers[:-1])
+# print(strings[:-3])
+# print(randomText[:-3])
+# strings.sort()
+# print(strings)
+
+human1 = {
+    "name": "Luka",
+    "age": 99,
+    "profession" : "QA",
+    "place_of_birth": "Celje"
+}
+
+human2 = {
+    "name": "Matej",
+    "age": 32,
+    "profession" : "FE Dev",
+    "kplace_of_birth": "Postojna"
+}
+
+human3 = {
+    "name": "Zan",
+    "age": 73,
+    "profession" : "Prod Manager",
+    "place_of_birth": "Ljubljana"
+}
+
+print(human1["name"])
+print(human3["place_of_birth"])
+
+human2["level"] = "senior"
+print(human2)
+
