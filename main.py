3521c7f5687d8d7bb3a609f8ba569d8cc78e1f2d
Luka Gaber <luka.gaber@dlabs.si>
2021-11-22 16:44:48 +0100

    add database

--- main.py
+++ main.py
@@ -1,14 +1,47 @@
-from flask import Flask
-from flask import Flask, render_template
-from flask import request, make_response
 
+from flask import Flask
+from flask import Flask, render_template, redirect, url_for
+from flask import request, make_response
+from sqla_wrapper import SQLAlchemy
 
 app = Flask(__name__)
+
+db = SQLAlchemy("sqlite:///db.sqlite")
+
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    author = db.Column(db.String, unique=False)
+    message = db.Column(db.String, unique=False)
+
+
+db.create_all()
 
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html",)
+
+
+@app.route("/chat")
+def chat():
+    messages = Message.query.all()
+    return render_template("chat.html", messages=messages)
+
+
+@app.route("/send-message", methods=["POST"])
+def saveMsg():
+    username = request.form.get("username")
+    message = request.form.get("message")
+
+    msg = Message(author=username, message=message)
+    db.session.add(msg)
+    db.session.commit()
+
+
+    print(f"User {username} send the following message: {message}.")
+    return redirect("/")
+
 
 
 @app.route("/test")
